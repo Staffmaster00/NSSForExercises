236fb253a63eca2f5e4e9bf9607736463422c014
Sam Staff <samstaff@Sams-MacBook-Pro.local>
2017-05-25 13:42:58 -0500

    first commit

---
+++ for-loops.js
@@ -0,0 +1,40 @@
+// Increment by 10 (incrementing by 10)
+
+// for (var i = 5; i < 120 ; i = i + 10) {
+// 	console.log(i);
+// }
+// ---------------------------------------------------------
+// Decrement by division (Dividing 4096 by 2)
+
+// for (var i = 4096; i >= 1; i = i/2) {
+// 	console.log(i);
+// }
+// ----------------------------------------------------------
+// Array interation (Presidents loop)
+
+// var presidents = ["Washington", "Adams", "Jefferson"];
+
+// for (var i = 0; i < presidents.length; i++) {
+// 	console.log(`${presidents[i]} was the #` + [i + 1] +" president.");
+// }
+// ------------------------------------------------------------
+// Object iteration (Ant species object loop)
+
+// var antSpecies = {
+//   argentine: {},
+//   army: {},
+//   bigHeaded: {},
+//   black: {},
+//   bull: {},
+//   carpenter: {},
+//   crazy: {},
+//   fire: {},
+//   glider: {},
+//   honeyPot: {},
+//   jackJumper: {}
+// }
+// for (var key in antSpecies) {
+// 	console.log(key)
+// }
+
+
